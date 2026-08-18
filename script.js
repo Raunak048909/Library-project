3613699c768a52024c05853d27f17fc5d3f4308d
Raunak  Tiwari <raunaktiwari@Raunaks-MacBook-Air-288.local>
2026-08-18 20:08:02 +0530

    added error in contructor if it is not called with new

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const booksFinished=document.querySelector(".books-finished");
 const unreadBooks=document.querySelector(".unread-books");
 const MyLibrary=[]
 function Book(title,author,pages,read){
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
     this.id = crypto.randomUUID(); 
     this.title=title;
     this.author=author;
